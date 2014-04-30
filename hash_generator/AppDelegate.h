2553b5f43b48cd61fb1dc3993badefa28545b395
Saurabh Mishra <srbmishra91@gmail.com>
2014-04-30 14:03:53 +0530

    Initial Commit

---
+++ hash_generator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hash_generator
+//
+//  Created by Saurabh Mishra on 4/30/14.
+//  Copyright (c) 2014 iquince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hash_generatorTests/hash_generatorTests.m
@@ -0,0 +1,34 @@
+//
+//  hash_generatorTests.m
+//  hash_generatorTests
+//
+//  Created by Saurabh Mishra on 4/30/14.
+//  Copyright (c) 2014 iquince. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hash_generatorTests : XCTestCase
+
+@end
+
+@implementation hash_generatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
